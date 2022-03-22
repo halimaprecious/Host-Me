eb1677172d261c576f93a285305369b82ee00a2f
jerikorir@gmail.com <jerikorir@gmail.com>
2022-03-22 22:54:24 +0300

    ft: added js buttons for checkout and its styling

--- js/location.js
+++ js/location.js
@@ -26,12 +26,12 @@ $(document).ready(function(){
     //proceed button
     $("#proceed").click(function(){
         
-        let pname = $("input#name").val();
+        var pname = $("input#name").val();
         let pidno = $("input#idno").val();
         let phone1 = $("input#phone1").val();
         let phone2 = $("input#phone2").val();
-        let plocation = $("#location option:selected").val();
-        let paccomodation = $("#accomodate option:selected").val();
+        var plocation = $("#location option:selected").val();
+        var paccomodation = $("#accomodate option:selected").val();
         let pperiod = $("input#period").val();
         let pdate = $("input#dateperiod").val();
 
@@ -71,8 +71,64 @@ $(document).ready(function(){
     //proceed to book
     $("#proceedtobook").click(function(){
         $("#sumtable").hide();
+        $(".pay").slideDown();
 
     });
+        //proceed to confirm mpesa
+        $("#confirm1").click(function(){
+            $("#myForm").hide();
+            // $(".final").show();
+            $("#sumtable").hide();
+            $(".pay").hide();
+            $(".thank").show();
 
+            var pname = $("input#name").val();
+            let pidno = $("input#idno").val();
+            let phone1 = $("input#phone1").val();
+            let phone2 = $("input#phone2").val();
+            var plocation = $("#location option:selected").val();
+            var paccomodation = $("#accomodate option:selected").val();
+            let pperiod = $("input#period").val();
+            let pdate = $("input#dateperiod").val();
+            
+            $("#thank1").append("Thank you "+ pname + " for allowing us to host you. "+"<br>"+ 
+            " Your payment of "+ paccomodation + " for a room located in "+ plocation + " for a period of " + pperiod + " month(s) , is supposed to paid be paid through M-Pesa and once we get your message you will "+ 
+            " receive our call");
+    
+        });
+        //proceed to confirm paypal
+        $("#confirm2").click(function(){
+            $("#myForm").hide();
+            // $(".final").show();
+            $("#sumtable").hide();
+            $(".pay").hide();
+            $(".thank2").show();
 
-})
+            var pname = $("input#name").val();
+            let pidno = $("input#idno").val();
+            let phone1 = $("input#phone1").val();
+            let phone2 = $("input#phone2").val();
+            var plocation = $("#location option:selected").val();
+            var paccomodation = $("#accomodate option:selected").val();
+            let pperiod = $("input#period").val();
+            let pdate = $("input#dateperiod").val();
+            
+            $("#thank2").append("Thank you "+ pname + " for allowing us to host you. "+"<br>"+ 
+            " Your payment of "+ paccomodation + " for a room located in "+ plocation + " for a period of " + pperiod + " month(s), is supposed to be paid using your paypal account and once we get your message you will "+ 
+            " receive our call");
+
+        });
+        //checkout button
+        $("#checkout1").click(function(){
+            $("#myForm").show();
+            $('#myForm')[0].reset();
+            $(".thank").hide();
+            $(".thank2").hide();
+        });
+        $("#checkout").click(function(){
+            $("#myForm").show();
+            $('#myForm')[0].reset();
+            $(".thank").hide();
+            $(".thank2").hide();
+        });
+});
